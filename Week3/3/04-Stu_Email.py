d87d5e69187497d232b97b5dd72c693f2b8f308c
Kash <ksantos79@gmail.com>
2018-05-07 22:37:20 -0700

    Activities from week 3

--- Week3/3/04-Stu_Email.py
+++ Week3/3/04-Stu_Email.py
@@ -18,21 +18,27 @@ with open(filepath,newline='') as csvfile:
 
 #FORMATTER:
 #variable = f"{data_item_1}.{data_item_2}@example.com"
+'''
 counter = 0
-
 print("First Name\tLast Name\t\tSSN\t\tEmail")
 for row2 in complete_list["First Name"]:
     print(complete_list["First Name"][counter]+"\t\t"+complete_list["Last Name"][counter]+"\t\t\t"+str(complete_list["SSN"][counter])+"\t"+str(complete_list["Email"][counter]))
     counter = counter +1
+'''
 #extract the file name from the path. "_" indicates the value of anything before filename
 _ , filename = os.path.split(filepath)
 counter = 0
 csvpath = os.path.join(".","output",filename)
-with open(csvpath,"w") as csvoutput:
-    field_names = ["Last Name","First Name","SSN","Email"]
-    writer = csv.DictWriter(csvoutput,fieldnames = field_names)
-    writer.writeheader()
-    for row3 in complete_list["First Name"]:
-        writer.writerow(complete_list["First Name"][counter]+","+complete_list["Last Name"][counter]+","+str(complete_list["SSN"][counter])+","+str(complete_list["Email"][counter]))
+with open(csvpath,"w",newline='') as csvoutput:
+    writer = csv.writer(csvoutput)
+    writer.writerow(complete_list.keys())
+    writer.writerow(complete_list.values())
 
+    """
+    field_names = ["Last Name","First Name","SSN","Email"]
+    writer = csv.DictWriter(csvoutput,fieldnames = field_names, dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
+    writer.writeheader()
+    for data in complete_list:
+        writer.writerow(data)
+"""
 print(_)
