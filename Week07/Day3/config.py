11f3ef3cbf739d07350d6a1e0a5952905d496c94
Kash <ksantos79@gmail.com>
2018-06-13 21:18:45 -0700

    Restore all activities in GitHub

---
+++ Week07/Day3/config.py
@@ -0,0 +1,8 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "KpHcZoJQx9Bfepm0EVNJo282I"
+consumer_secret = "aM7qRHNitlWj0R1ln6QzvvGl18rEV2Pa4VO16A7nDs03Pf7uM9"
+access_token = "777278389-489Ii264R59J4nkf7rGMnkGLFMNxHq5RuDcN87V3"
+access_token_secret = "Ysiqd45nZPoIZ7nC2g9dU3IZCUnajLvNdB9QEtgUPEhbN"
+
+weather_api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
